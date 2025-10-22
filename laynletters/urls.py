360315f1a118f41a8074da3ceb8fd6ea49674676
limcaroline <carolineloislim@gmail.com>
2025-10-22 19:11:52 +0200

    added to folder and file structure

--- laynletters/urls.py
+++ laynletters/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('', include('core.urls')),
+    path('stories/', include('stories.urls')),
+    path('itineraries/', include('itineraries.urls')),
+    path('reviews/', include('reviews.urls')),
+    path('budgets/', include('budgets.urls')),
 ]
