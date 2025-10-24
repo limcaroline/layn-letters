cef1412dca65ed3a19d6bcc5e3612ff01ce8ed61
limcaroline <carolineloislim@gmail.com>
2025-10-24 12:53:55 +0200

    added CommentForm

--- stories/views.py
+++ stories/views.py
@@ -1,14 +1,15 @@
 from django.contrib.auth.mixins import (
     LoginRequiredMixin, UserPassesTestMixin
 )
-from django.db.models import Sum
 from django.http import JsonResponse
 from django.urls import reverse_lazy
 from django.views import View
+from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import (
     ListView, DetailView, CreateView, UpdateView, DeleteView
 )
-from .models import Story, Vote
+from .models import Story, Vote, Comment, CommentVote
+from django.db.models import Sum
 from core.utils import is_site_owner
 
 
@@ -73,4 +74,33 @@ class VoteToggle(LoginRequiredMixin, View):
             vote.value = value
             vote.save()
         score = story.votes.aggregate(total=Sum("value"))["total"] or 0
+        return JsonResponse({"score": score})
+
+
+class CommentEdit(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Comment
+    fields = ["body"]
+    def test_func(self): return self.get_object().author == self.request.user
+    def get_success_url(self): return self.object.story.get_absolute_url()
+
+
+class CommentDelete(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Comment
+    def test_func(self):
+        c = self.get_object()
+        return c.author == self.request.user or is_site_owner(self.request.user)
+    def get_success_url(self): return self.object.story.get_absolute_url()
+
+
+class CommentVoteToggle(LoginRequiredMixin, View):
+    def post(self, request, pk):
+        comment = get_object_or_404(Comment, pk=pk)
+        value = int(request.POST.get("value", 1))
+        vote, created = CommentVote.objects.get_or_create(
+            comment=comment, user=request.user, defaults={"value": value}
+        )
+        if not created:
+            vote.value = value
+            vote.save()
+        score = comment.votes.aggregate(total=Sum("value"))["total"] or 0
         return JsonResponse({"score": score})
